4f1818c478af77ef8ea3cc1e51d35fb34a77bcd4
BoomReality <qw15599610202@sina.com>
2023-06-06 17:59:57 +0800

    修改查询员工表分页方式

--- springbootFirstProject/springbootFirstProject/src/main/java/com/apex/mapper/EmpMapper.java
+++ springbootFirstProject/springbootFirstProject/src/main/java/com/apex/mapper/EmpMapper.java
@@ -8,9 +8,12 @@ import java.util.List;
 
 @Mapper
 public interface EmpMapper {
-    @Select("select * from emp limit #{start},#{pageSize}")
-    public List<Emp> selList(Integer start, Integer pageSize);
+//    @Select("select * from emp limit #{start},#{pageSize}")
+//    public List<Emp> selList(Integer start, Integer pageSize);
+//
+//    @Select("select count(*) from emp")
+//    public Long count();
 
-    @Select("select count(*) from emp")
-    public Long count();
+    @Select("select * from emp")
+    public List<Emp> list();
 }
--- springbootFirstProject/springbootFirstProject/src/main/java/com/apex/service/impl/EmpImpl.java
+++ springbootFirstProject/springbootFirstProject/src/main/java/com/apex/service/impl/EmpImpl.java
@@ -4,6 +4,8 @@ import com.apex.mapper.EmpMapper;
 import com.apex.pojo.Emp;
 import com.apex.pojo.PageBean;
 import com.apex.service.EmpServer;
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +15,33 @@ import java.util.List;
 public class EmpImpl implements EmpServer {
     @Autowired
     EmpMapper empMapper;
+
+    //    @Override
+//    public PageBean empList(Integer page, Integer pageSize) {
+////      1.获取记录总数
+//        Long count = empMapper.count();
+//
+////      2.获取分页查询结果
+//        Integer strat = (page -1) * pageSize;
+//        List<Emp> empList = empMapper.selList(strat,pageSize);
+//
+////      3.封装总记录数和查询结果
+//        PageBean pageBean = new PageBean(count,empList);
+//
+//        return pageBean;
+//    }
     @Override
     public PageBean empList(Integer page, Integer pageSize) {
-//      1.获取记录总数
-        Long count = empMapper.count();
+//        1.设置分页
+        PageHelper.startPage(page, pageSize);
 
-//      2.获取分页查询结果
-        Integer strat = (page -1) * pageSize;
-        List<Emp> empList = empMapper.selList(strat,pageSize);
+//        2.执行查询
+        List<Emp> empList = empMapper.list();
+        Page<Emp> p = (Page<Emp>) empList;
 
-//      3.封装总记录数和查询结果
-        PageBean pageBean = new PageBean(count,empList);
-
+//        3.封装PageBean对象
+        PageBean pageBean = new PageBean(p.getTotal(), p.getResult());
         return pageBean;
+
     }
 }
